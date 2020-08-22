d3fc845a8bad73e53dde76d9306056d2196039bd
Ayush Soni <ayushakshay.soni@gmail.com>
2020-08-22 11:48:09 +0530

    Updates

---
+++ precache-manifest.8e584fd697fc691320affedcfc73a726.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20abff82b9f889a271d67fbc9ff3be8",
+    "url": "/News_Reader_App/index.html"
+  },
+  {
+    "revision": "00bda61380dc11897f7a",
+    "url": "/News_Reader_App/static/css/main.2d9ddc0a.chunk.css"
+  },
+  {
+    "revision": "5f5aef2ca0258b646d97",
+    "url": "/News_Reader_App/static/js/2.8bd31e08.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/News_Reader_App/static/js/2.8bd31e08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00bda61380dc11897f7a",
+    "url": "/News_Reader_App/static/js/main.073a694e.chunk.js"
+  },
+  {
+    "revision": "ede671f5bbd5ab3731d0",
+    "url": "/News_Reader_App/static/js/runtime-main.045dc4d4.js"
+  }
+]);
